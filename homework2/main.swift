91247d9eaf5611f326327d68395efc05387bb28b
bombag456 <bombag456@gmail.com>
2022-09-15 03:44:15 +0600

    dqdq

--- homework2/main.swift
+++ homework2/main.swift
@@ -9,3 +9,40 @@ import Foundation
 
 print("Hello, World!")
 
+
+func printname(name:String){
+    print("Меня зовут \(name)")
+          
+}
+print("Как вас зовут?")
+let name = readLine()!
+          
+printname(name: name)
+
+func printsurname(surname:String){
+    print("Моя Фамилия-\(surname)")
+}
+print("Ваша фамилия")
+let surname=readLine()!
+printsurname(surname: surname)
+
+func age(age:String){
+    print("МНЕ\(age)")
+}
+print("Сколько вам лет")
+let age=readLine()!
+age(age: age)
+
+func born(born:String){
+    print("Я живу \(born)")
+}
+print("Где вы живете")
+let born=readLine()!
+born(born: born)
+
+func number(number:String){
+    print("Мой номер \(number)")
+}
+print("Можно ваш номер?")
+let number=readLine()!
+number(number: number)
